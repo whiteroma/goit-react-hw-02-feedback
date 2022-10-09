6fd6cb169ec0fec08c816d304b21e705d6e47ff2
whiteroma <roma.white95@gmail.com>
2022-10-09 16:31:48 +0200

    step 2 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import PropTypes from 'prop-types';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    percentage: 0,
+  };
+
+  static propTypes = {};
+
+  onGood = () => {
+    this.setState(prev => ({
+      good: prev.good + 1,
+    }));
+  };
+
+  onNeutral = () => {
+    this.setState(prev => ({
+      neutral: prev.neutral + 1,
+    }));
+  };
+
+  onBad = () => {
+    this.setState(prev => ({
+      bad: prev.bad + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+        <FeedbackOptions
+          onGood={this.onGood}
+          onNeutral={this.onNeutral}
+          onBad={this.onBad}
+        />
+        </Section>
+        <Section title="Statistics">
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.state.total}
+          percentage={this.state.percentage}
+        />
+        </Section>
+      </>
+    );
+  }
+}
